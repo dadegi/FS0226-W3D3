2d2f1944b7ce7ba5733192c4eecb53de2884aae5
Naviganet <dadegi@gmail.com>
2026-05-20 12:35:18 +0200

    Codici lezione U1-W3-D3

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@ const printH2 = document.querySelector('#printH2');
 const sum = document.querySelector('#sum');
 const sumResult = document.querySelector('#sumResult');
 const otherSum = document.querySelector('#otherSum');
+const userNameInput = document.querySelector('#userNameInput');
+const inputNameValue = document.querySelector('#inputNameValue');
+const userSurnameInput = document.querySelector('#userSurnameInput');
+const inputSurnameValue = document.querySelector('#inputSurnameValue');
+const choose = document.querySelector('#choose');
+const chooseValue = document.querySelector('#chooseValue');
+const myCheck = document.querySelector('#myCheck');
+const checkValue = document.querySelector('#checkValue');
 
 messageLog.addEventListener('click', (e) => {
 	console.log('Hai cliccato!');
@@ -19,15 +27,15 @@ printH2.addEventListener('click', (e) => {
 
 sum.addEventListener('click', (e) => {
 	sumNumbers(8, 12, e);
-    firstLog();
-    secondLog();
+	firstLog();
+	secondLog();
 });
 
 function sumNumbers(num1, num2, event = Event) {
 	let resultSum = num1 + num2;
 	if (event.type === 'click') {
 		sumResult.textContent = `Ho scritto in sumResult questo risultato: ${resultSum}`;
-        otherSum.textContent = ' ';
+		otherSum.textContent = ' ';
 	} else {
 		otherSum.textContent = `Ho scritto in otherSum questo risultato: ${resultSum}`;
 	}
@@ -36,9 +44,80 @@ function sumNumbers(num1, num2, event = Event) {
 sumNumbers(4, 9);
 
 const firstLog = () => {
-    console.log('Primo Log');
-}
+	console.log('Primo Log');
+};
 
 const secondLog = () => {
-    console.log('Secondo Log');
-}
+	console.log('Secondo Log');
+};
+
+userNameInput.addEventListener('input', (e) => {
+	inputNameValue.textContent = e.target.value;
+});
+
+userSurnameInput.addEventListener('change', (e) => {
+	inputSurnameValue.textContent = e.target.value;
+});
+
+choose.addEventListener('change', (e) => {
+	chooseValue.textContent = e.target.value;
+});
+
+myCheck.addEventListener('change', (e) => {
+	checkValue.textContent = e.target.value;
+});
+
+// Validazione form
+const myName = document.querySelector('#myName');
+const mySurname = document.querySelector('#mySurname');
+const myAge = document.querySelector('#myAge');
+const myEmail = document.querySelector('#myEmail');
+const sendForm = document.querySelector('#sendForm');
+
+const myPerson = {
+	nome: '',
+	cognome: '',
+	eta: 0,
+	email: '',
+};
+
+myName.addEventListener('blur', (e) => {
+	const errorName = document.querySelector('#errorName');
+	if (e.target.value === '') {
+		errorName.textContent = 'Compilare il campo nome!';
+	} else {
+		myPerson.nome = e.target.value;
+		errorName.textContent = ' ';
+	}
+});
+
+mySurname.addEventListener('blur', (e) => {
+	const errorSurname = document.querySelector('#errorSurname');
+	if (e.target.value === '') {
+		errorSurname.textContent = 'Compilare il campo cognome!';
+	} else {
+		errorSurname.textContent = ' ';
+		myPerson.cognome = e.target.value;
+	}
+});
+
+myEmail.addEventListener('blur', (e) => {
+	const errorEmail = document.querySelector('#errorEmail');
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const validEmail = regex.test(e.target.value);
+	if (e.target.value === '') {
+		errorEmail.textContent = 'Compilare il campo email!';
+	} else if (validEmail === false) {
+		errorEmail.textContent = 'Inserire una email valida!';
+	} else {
+		myPerson.email = e.target.value;
+		errorEmail.textContent = ' ';
+	}
+});
+
+sendForm.addEventListener('click', (e) => {
+	e.preventDefault();
+	const myAge = document.querySelector('#myAge');
+	myPerson.eta = myAge.value;
+	console.log(myPerson);
+});
